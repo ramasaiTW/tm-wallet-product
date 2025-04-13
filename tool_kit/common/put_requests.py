2d48ec062ea327e93d440a29a08bee14530bf4f4
psowmyatw <sitasowmya.paluri@thoughtworks.com>
2025-04-13 12:29:14 +0530

    updated put requests files reformatted

--- tool_kit/common/put_requests.py
+++ tool_kit/common/put_requests.py
@@ -33,7 +33,6 @@ def update_account_schedule_tag(
 def update_account_migration(
     account_migration_id=None, request_id=None, status=None, paths=None
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "account_migration": {
@@ -52,16 +51,15 @@ def update_account_migration(
 
 
 def update_account(
-    account_id = None,
-    status = None,
-    closing_timestamp = None,
-    stakeholder_ids = None,
-    paths = None,
-    restriction_set_ids = None,
-    restriction_set_definition_ids = None,
-    restriction_set_definition_version_ids = None,
+    account_id=None,
+    status=None,
+    closing_timestamp=None,
+    stakeholder_ids=None,
+    paths=None,
+    restriction_set_ids=None,
+    restriction_set_definition_ids=None,
+    restriction_set_definition_version_ids=None,
 ) -> List[Any]:
-
     request_body = {
         "account_id": account_id,
         "account": {
@@ -85,17 +83,16 @@ def update_account(
     return api_request(
         "put",
         f"/v1/accounts/{account_id}",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
 def update_account_details(
-    request_id = None,
-    account_id = None,
-    items_to_add = None,
-    items_to_remove = None,
+    request_id=None,
+    account_id=None,
+    items_to_add=None,
+    items_to_remove=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "account_id": account_id,
@@ -106,18 +103,16 @@ def update_account_details(
     return api_request(
         "put",
         f"/v1/accounts/{account_id}:updateDetails",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_service_account(
-    service_account_id = None,
-    status = None,
-    paths = None,
-    refresh_token = None,
+    service_account_id=None,
+    status=None,
+    paths=None,
+    refresh_token=None,
 ) -> List[Any]:
-
     request_body = {
         "service_account.id": service_account_id,
         "service_account": {
@@ -134,18 +129,16 @@ def update_service_account(
     return api_request(
         "put",
         f"/v1/service-accounts/{service_account_id}",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_service_account_permissions(
-    request_id = None,
-    service_account_id = None,
-    items_to_add = None,
-    items_to_remove = None,
+    request_id=None,
+    service_account_id=None,
+    items_to_add=None,
+    items_to_remove=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "items_to_add": items_to_add,
@@ -155,19 +148,17 @@ def update_service_account_permissions(
     return api_request(
         "put",
         f"/v1/service-accounts/{service_account_id}:updatePermissions",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_calendar_event(
-    request_id = None,
-    calendar_event_id = None,
-    name = None,
-    is_active = None,
-    paths = None,
+    request_id=None,
+    calendar_event_id=None,
+    name=None,
+    is_active=None,
+    paths=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "calendar_event": {
@@ -182,18 +173,16 @@ def update_calendar_event(
     return api_request(
         "put",
         f"/v1/calendar-event/{calendar_event_id}:updateDetails",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_bookkeeping_date(
-    request_id = None,
-    calender_id = None,
-    date = None,
-    paths = None,
+    request_id=None,
+    calender_id=None,
+    date=None,
+    paths=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "bookkeeping_date": {
@@ -207,17 +196,16 @@ def update_bookkeeping_date(
     return api_request(
         "put",
         f"/v1/calendar/bookkeeping-date/{calender_id}",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
 def change_current_calendar_period(
-    request_id = None,
-    calendar_id = None,
-    action = None,
-    action_timestamp = None,
+    request_id=None,
+    calendar_id=None,
+    action=None,
+    action_timestamp=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "action": action,
@@ -227,20 +215,18 @@ def change_current_calendar_period(
     return api_request(
         "put",
         f"/v1/calendar/{calendar_id}/period/current:change",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_calendar(
-    request_id = None,
-    calendar_id = None,
-    is_active = None,
-    display_name = None,
-    description = None,
-    paths = None,
+    request_id=None,
+    calendar_id=None,
+    is_active=None,
+    display_name=None,
+    description=None,
+    paths=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "calendar": {
@@ -256,28 +242,26 @@ def update_calendar(
     return api_request(
         "put",
         f"/v1/calendar/{calendar_id}:updateDetails",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_customer_address(
-    request_id = None,
-    customer_address_id = None,
-    house_name = None,
-    street_number = None,
-    street = None,
-    local_municipality = None,
-    city = None,
-    postal_area = None,
-    governing_district = None,
-    country = None,
-    address_type = None,
-    start_timestamp = None,
-    end_timestamp = None,
-    paths = None,
+    request_id=None,
+    customer_address_id=None,
+    house_name=None,
+    street_number=None,
+    street=None,
+    local_municipality=None,
+    city=None,
+    postal_area=None,
+    governing_district=None,
+    country=None,
+    address_type=None,
+    start_timestamp=None,
+    end_timestamp=None,
+    paths=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "customer_address": {
@@ -301,45 +285,46 @@ def update_customer_address(
     return api_request(
         "put",
         f"/v1/customer-addresses/{customer_address_id}",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
-
 def update_customer(
-    request_id = None,
-    customer_id = None,
-    status = None,
-    identifiers = None,
-    customer_details_title = None,
-    customer_details_first_name = None,
-    customer_details_middle_name = None,
-    customer_details_last_name = None,
-    customer_details_dob = None,
-    customer_details_gender = None,
-    customer_details_nationality = None,
-    customer_details_email_address = None,
-    customer_details_mobile_phone_number = None,
-    customer_details_home_phone_number = None,
-    customer_details_business_phone_number = None,
-    customer_details_contact_method = None,
-    customer_details_country_of_residence = None,
-    customer_details_country_of_taxation = None,
-    customer_details_accessibility = None,
-    customer_details_external_customer_id = None,
-    identifiers_type_email = None,
-    identifiers_type_username = None,
-    identifiers_type_phone = None,
-    paths = None,
+    request_id=None,
+    customer_id=None,
+    status=None,
+    identifiers=None,
+    customer_details_title=None,
+    customer_details_first_name=None,
+    customer_details_middle_name=None,
+    customer_details_last_name=None,
+    customer_details_dob=None,
+    customer_details_gender=None,
+    customer_details_nationality=None,
+    customer_details_email_address=None,
+    customer_details_mobile_phone_number=None,
+    customer_details_home_phone_number=None,
+    customer_details_business_phone_number=None,
+    customer_details_contact_method=None,
+    customer_details_country_of_residence=None,
+    customer_details_country_of_taxation=None,
+    customer_details_accessibility=None,
+    customer_details_external_customer_id=None,
+    identifiers_type_email=None,
+    identifiers_type_username=None,
+    identifiers_type_phone=None,
+    paths=None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "customer": {
             "status": status,
             "identifiers": [
                 {"identifier_type": "IDENTIFIER_TYPE_EMAIL", "identifier": identifiers_type_email},
-                {"identifier_type": "IDENTIFIER_TYPE_USERNAME", "identifier": identifiers_type_username},
+                {
+                    "identifier_type": "IDENTIFIER_TYPE_USERNAME",
+                    "identifier": identifiers_type_username,
+                },
                 {"identifier_type": "IDENTIFIER_TYPE_PHONE", "identifier": identifiers_type_phone},
             ],
             "customer_details": {
@@ -369,7 +354,7 @@ def update_customer(
     return api_request(
         "put",
         f"/v1/customers/{customer_id}",
-        data = json.dumps(request_body),
+        data=json.dumps(request_body),
     )
 
 
@@ -379,7 +364,6 @@ def update_customer_additional_details(
     items_to_add: dict = None,
     items_to_remove: list = None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "items_to_add": items_to_add,
@@ -393,14 +377,12 @@ def update_customer_additional_details(
     )
 
 
-
 def update_flag_definition(
     request_id: str = None,
     flag_definition_id: str = None,
     is_active: bool = None,
     paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "flag_definition": {"is_active": is_active},
         "request_id": request_id,
@@ -412,7 +394,6 @@ def update_flag_definition(
     )
 
 
-
 def update_flag(
     flag_id: str = None,
     request_id: str = None,
@@ -420,7 +401,6 @@ def update_flag(
     is_active: bool = None,
     paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "flag": {"description": description, "is_active": is_active},
         "request_id": request_id,
@@ -437,7 +417,6 @@ def update_parameter_value_hierarchy_node(
     metadata: dict = None,
     paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "parameter_value_hierarchy_node": {"name": name, "metadata": metadata},
@@ -451,7 +430,6 @@ def update_parameter_value_hierarchy_node(
     )
 
 
-
 def update_parameter_value(
     request_id: str = None,
     parameter_value_id: str = None,
@@ -459,9 +437,8 @@ def update_parameter_value(
     is_cancelled: bool = None,
     paths: list = None,
     skip_pre_parameter_change_hook: bool = None,
-    set_to_now: bool = None
+    set_to_now: bool = None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "parameter_value": {
@@ -469,7 +446,9 @@ def update_parameter_value(
             "is_cancelled": is_cancelled,
         },
         "update_mask": {"paths": paths},
-        "is_cancelled_update_options": {"skip_pre_parameter_change_hook": skip_pre_parameter_change_hook},
+        "is_cancelled_update_options": {
+            "skip_pre_parameter_change_hook": skip_pre_parameter_change_hook
+        },
         "effective_to_timestamp_update_options": {"set_to_now": set_to_now},
     }
 
@@ -478,14 +457,12 @@ def update_parameter_value(
     )
 
 
-
 def update_payment_device_link(
     payment_device_link_id: str = None,
     request_id: str = None,
     status: str = "PAYMENT_DEVICE_LINK_STATUS_ACTIVE",
-    paths: list = None
+    paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "payment_device_link": {"status": status},
         "request_id": request_id,
@@ -497,7 +474,6 @@ def update_payment_device_link(
     )
 
 
-
 def update_payment_device(
     payment_device_id: str = None,
     request_id: str = None,
@@ -505,9 +481,8 @@ def update_payment_device(
     paths: list = None,
     restriction_set_ids: list = None,
     restriction_set_definition_ids: list = None,
-    restriction_set_definition_version_ids: list = None
+    restriction_set_definition_version_ids: list = None,
 ) -> List[Any]:
-
     request_body = {
         "payment_device": {"status": status},
         "request_id": request_id,
@@ -527,15 +502,13 @@ def update_payment_device(
     )
 
 
-
 def update_policy(
     policy_id: str = None,
     request_id: str = None,
     description: str = None,
     rego_source: str = None,
-    paths: list = None
+    paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "request_id": request_id,
         "policy": {"description": description, "rego_source": rego_source},
@@ -551,14 +524,16 @@ def update_postings_apiclient(
     client_id: str = None,
     response_topic: str = None,
     response_topic_low_priority: str = None,
-    update_mask_paths: list = None
+    update_mask_paths: list = None,
 ) -> List[Any]:
     request_body = {
         "request_id": request_id,
         "postings_api_client": {
             "client_id": client_id,
-            "response_topic": response_topic or "integration.postings_api.domestic_payment_processor_id.response",
-            "response_topic_low_priority": response_topic_low_priority or "integration.postings_api.domestic_payment_processor_id.low_priority.response",
+            "response_topic": response_topic
+            or "integration.postings_api.domestic_payment_processor_id.response",
+            "response_topic_low_priority": response_topic_low_priority
+            or "integration.postings_api.domestic_payment_processor_id.low_priority.response",
         },
         "update_mask": {"paths": update_mask_paths or ["response_topic_low_priority"]},
     }
@@ -579,7 +554,7 @@ def update_processing_group(
         "request_id": request_id,
         "processing_group": {
             "timezone": timezone,
-            "status": status or "PROCESSING_GROUP_STATUS_ACTIVE"
+            "status": status or "PROCESSING_GROUP_STATUS_ACTIVE",
         },
         "update_mask": {"paths": update_mask_paths},
         "minimum_observation_timestamp_update_options": {
@@ -592,13 +567,12 @@ def update_processing_group(
     )
 
 
-
 def update_product_version_params(
     product_version_id=None,
-    request_id = None,
-    name = None,
-    value = None,
-    effective_timestamp = None,
+    request_id=None,
+    name=None,
+    value=None,
+    effective_timestamp=None,
 ) -> List[Any]:
     request_body = {
         "request_id": request_id,
@@ -629,9 +603,8 @@ def update_restriction_set(
     restriction_set_id: str = None,
     request_id: str = None,
     is_active: bool = True,
-    update_mask_paths: list = None
+    update_mask_paths: list = None,
 ) -> List[Any]:
-
     request_body = {
         "restriction_set": {"is_active": is_active or True},
         "request_id": request_id,
